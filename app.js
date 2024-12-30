ea48d5c11f7df0b90f6b0099b8d421674bf8e310
aaadi02 <adityadakhane011@gmail.com>
2024-12-30 22:36:38 +0530

    Implementing the users routes. This routes are not complete yet we will do it in future

--- app.js
+++ app.js
@@ -103,9 +103,52 @@ const deleteTour = (req, res) => {
   });
 };
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not defined yet",
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not defined yet",
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This route is not defined yet",
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).josn({
+    status: "error",
+    message: "This route is not defined yet",
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).josn({
+    status: "error",
+    message: "This route is not defined yet",
+  });
+};
+
 app.route("/api/v1/tours").get(getAllTours).post(createTour);
 
 app.route("api/v1/tours/:id").get(getTour).patch(updateTour).delete(deleteTour);
+
+app.route("/api/v1/users").get(getAllUsers).post(createUser);
+
+app
+  .route("/api/v1/users/:id")
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 const port = 3000;
 app.listen(port, () => {
