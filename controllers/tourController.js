afb08e1bc3ab655f962b56f377674a6d85e77a58
aaadi02 <adityadakhane011@gmail.com>
2024-12-31 10:06:10 +0530

    Learning chaining of multiple middleware with the help of check body middileware where we need to check if post request contains name or price or not

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -15,6 +15,16 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: "fail",
+      message: "Missing tour name or price of tour",
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.reqTime);
   res.status(200).json({
